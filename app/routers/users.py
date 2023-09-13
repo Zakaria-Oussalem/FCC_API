9129d882515206619ca33199e4f59815b3b800bb
Zakaria Oussalem <Zakaria.Oussalem.pro@gmail.com>
2023-09-13 23:27:27 +0200

    Add perfixes to routes

--- app/routers/users.py
+++ app/routers/users.py
@@ -5,10 +5,10 @@ import models
 from database import get_db
 from utils import hash
 
-router = APIRouter()
+router = APIRouter(prefix="/users")
 
 
-@router.post("/users", status_code=status.HTTP_201_CREATED, response_model=UserOurt)
+@router.post("/", status_code=status.HTTP_201_CREATED, response_model=UserOurt)
 def create_user(user: UserCreate, db: Session = Depends(get_db)):
     # hash the password - user.password
     hashedPassword = hash(user.password)
@@ -23,7 +23,7 @@ def create_user(user: UserCreate, db: Session = Depends(get_db)):
     return new_user
 
 
-@router.get("/users/{id}", response_model=UserOurt)
+@router.get("/{id}", response_model=UserOurt)
 def get_user(id: int, db: Session = Depends(get_db)):
     user = db.query(models.User).filter(models.User.id == id).first()
 
