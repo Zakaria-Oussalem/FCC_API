a7e795d27ee49d089398ad1432daa2d73d6aeda1
Zakaria Oussalem <Zakaria.Oussalem.pro@gmail.com>
2023-09-15 20:28:41 +0200

    Add creation of jwt token to authentification

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -4,15 +4,20 @@ import models
 from schemas import UserLogIn
 from database import get_db
 from utils import hash, verify
+from oauth2 import create_access_token
+from fastapi.security.oauth2 import OAuth2PasswordRequestForm
 
 router = APIRouter(tags=["Authentication"])
 
 
 @router.post("/login")
-def login(user_credentials: UserLogIn, db: Session = Depends(get_db)):
+def login(
+    user_credentials: OAuth2PasswordRequestForm = Depends(),
+    db: Session = Depends(get_db),
+):
     user = (
         db.query(models.User)
-        .filter(models.User.email == user_credentials.email)
+        .filter(models.User.email == user_credentials.username)
         .first()
     )
 
@@ -22,12 +27,12 @@ def login(user_credentials: UserLogIn, db: Session = Depends(get_db)):
         )
 
     # Check if password is correct
-    print(user.password)
-    print(hash(user_credentials.password))
     if not verify(user_credentials.password, user.password):
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND, detail=f"Invalid Credentials"
         )
 
     # Create a token
-    return {"token": "example token"}
+    access_token = create_access_token(data={"user_id": user.id})
+
+    return {"access_token": access_token, "token_type": "bearer"}
