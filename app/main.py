aa10769e2ab727ad0154d31b3848a23823286039
Zakaria Oussalem <Zakaria.Oussalem.pro@gmail.com>
2023-10-01 19:14:44 +0200

    fix import

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from routers import posts, users, auth, votes
+from .routers import posts, users, auth, votes
 from fastapi.middleware.cors import CORSMiddleware
 
 # models.Base.metadata.create_all(bind=engine)
